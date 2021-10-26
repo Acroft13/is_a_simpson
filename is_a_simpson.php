f6048f2b02795d7835e87fbdb8a3d54ec443221a
Amy <71037651+Acroft13@users.noreply.github.com>
2021-10-26 15:41:09 -0700

    Add is_a_simpson php file

---
+++ is_a_simpson.php
@@ -0,0 +1,31 @@
+<html lang="en">
+<!--/*Author: Amy Croft
+Assignment: 8
+Course: DGL123
+Date: October 26, 2021*/-->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Is a Simpson</Index></title>
+</head>
+
+<body>
+
+<?php
+
+function is_a_simpson($full_name) {
+    // Complete this function
+}
+
+var_dump(is_a_simpson('Lisa Simpson')); // This should return true
+var_dump(is_a_simpson('Marge Simpson')); // This should return true
+var_dump(is_a_simpson('Ned Flanders')); // This should return false
+var_dump(is_a_simpson('Montgomery Burns')); // This should return false
+var_dump(is_a_simpson('Simpson Harris')); // This should return false
+
+
+
+?>
+</body>
+</html>
